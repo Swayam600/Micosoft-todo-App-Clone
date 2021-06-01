b8e0ebb60f12046205504897c4d0ad32bbda9b26
Swayam <71008174+Swayam600@users.noreply.github.com>
2021-06-01 12:04:19 +0530

    functional enough

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,12 +23,35 @@ checkboxes.forEach(checkbox => {
     })
 })
 
-// DELETE FUNCTIONALITY
-const deleteButtons = document.querySelectorAll('.delete')
 
-deleteButtons.forEach(deletebutton => {
-    deletebutton.addEventListener('click', () => {
-        const parent = deletebutton.parentElement
-        parent.remove()
+// TODO ADD FUNCTIONALITY
+
+const addButton = document.querySelector('.add-button')
+const input = document.querySelector('.input')
+const list = document.querySelector('.list ul')
+
+addButton.addEventListener('click', () => {
+    const newLI = document.createElement('li')
+
+    const checkBox = document.createElement('div')
+    checkBox.classList.add('checkbox')
+
+    const span = document.createElement('span')
+    span.innerText = input.value
+
+    const delete_ = document.createElement('button')
+    delete_.classList.add('delete')
+    delete_.innerText = 'Delete'
+
+    // DELETE FUNCTIONALITY
+    delete_.addEventListener('click', function() {
+        newLI.remove()
     })
+
+    newLI.appendChild(checkBox)
+    newLI.appendChild(span)
+    newLI.appendChild(delete_)
+
+    list.appendChild(newLI)
+
 })
